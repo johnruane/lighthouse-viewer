d14eb1e8120b230c6910f10b15c801b408968739
John Ruane <john.ruane@nbrown.co.uk>
2022-02-07 20:13:58 +0000

    dep disable

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useState} from 'react';
 import Table from './Table';
 import { cleanBuildData, filterAndGroupData } from './lib/helpers';
